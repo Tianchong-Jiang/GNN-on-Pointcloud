092ff2e77527c0030a2d14c632dc392b84d8fbb0
Tianchong Jiang <tianchongjiang@gmail.com>
2023-03-09 15:02:54 -0600

    added functions to corrupt pointcloud

--- data.py
+++ data.py
@@ -14,6 +14,11 @@ import glob
 import h5py
 import numpy as np
 from torch.utils.data import Dataset
+from params_proto import ParamsProto
+from params_proto import Proto
+from scipy.spatial.transform import Rotation as R
+import einops
+import wandb
 
 
 def download():
@@ -24,7 +29,7 @@ def download():
     if not os.path.exists(os.path.join(DATA_DIR, 'modelnet40_ply_hdf5_2048')):
         www = 'https://shapenet.cs.stanford.edu/media/modelnet40_ply_hdf5_2048.zip'
         zipfile = os.path.basename(www)
-        os.system('wget %s; unzip %s' % (www, zipfile))
+        os.system('wget %s --no-check-certificate; unzip %s' % (www, zipfile))
         os.system('mv %s %s' % (zipfile[:-4], DATA_DIR))
         os.system('rm %s' % (zipfile))
 
@@ -50,7 +55,7 @@ def load_data(partition):
 def translate_pointcloud(pointcloud):
     xyz1 = np.random.uniform(low=2./3., high=3./2., size=[3])
     xyz2 = np.random.uniform(low=-0.2, high=0.2, size=[3])
-       
+
     translated_pointcloud = np.add(np.multiply(pointcloud, xyz1), xyz2).astype('float32')
     return translated_pointcloud
 
@@ -62,26 +67,99 @@ def jitter_pointcloud(pointcloud, sigma=0.01, clip=0.02):
 
 
 class ModelNet40(Dataset):
-    def __init__(self, num_points, partition='train'):
+    def __init__(self, partition = 'test', operation = 'noise'):
         self.data, self.label = load_data(partition)
-        self.num_points = num_points
-        self.partition = partition        
+        self.partition = partition
+        self.operation = operation
+
+        self.op_dict = {
+            'rigid': self.rigid,
+            'noise': self.noise,
+            'remove_local': self.remove_local,
+            'warp': self.warp,
+            'drop_uniform': self.drop_uniform,
+            'drop': self.drop
+                      }
 
     def __getitem__(self, item):
-        pointcloud = self.data[item][:self.num_points]
+        pointcloud = self.data[item]
         label = self.label[item]
-        if self.partition == 'train':
-            pointcloud = translate_pointcloud(pointcloud)
-            np.random.shuffle(pointcloud)
+
+        np.random.shuffle(pointcloud)
+        pointcloud = self.rigid(pointcloud)
+
+        pointcloud = self.op_dict[self.operation](pointcloud)
+
         return pointcloud, label
 
     def __len__(self):
         return self.data.shape[0]
 
+    def visualize(self, num_samples = 10):
+        print('visualizing pointcloud on wandb...')
+        # Visualize pointcloud on Wandb
+        for i in range(num_samples):
+            pointcloud, label = self.__getitem__(i)
+            # import pdb; pdb.set_trace()
+            wandb.log({f'label:{label}': wandb.Object3D(pointcloud)}, step = i)
+        print('visualizing pointcloud on wandb...done')
+
+    def rigid(self, pointcloud, scale = 1):
+        rot = R.random().as_matrix()
+        trans = np.random.rand(3) * scale
+
+        pointcloud = einops.rearrange(pointcloud, 'n d -> d n')
+        pointcloud = rot @ pointcloud
+        pointcloud = einops.rearrange(pointcloud, 'd n -> n d')
+
+        pointcloud = np.add(trans, pointcloud)
+
+        return pointcloud
+
+    def noise(self, pointcloud, scale = 0.02):
+        trans = np.random.randn(*pointcloud.shape) * scale
+        pointcloud = pointcloud + trans
+        return pointcloud
+
+    def remove_local(self, pointcloud, scale = 0.1):
+        idx = np.random.randint(pointcloud.shape[0])
+        center = pointcloud[idx]
+        pointcloud = pointcloud[np.linalg.norm(pointcloud - center, axis=1) > scale]
+        return pointcloud
+
+    def warp(self, pointcloud, scale = 1.1):
+        pointcloud = np.power(pointcloud, scale)
+        return pointcloud
+
+    def drop_uniform(self, pointcloud, prob = 0.5):
+        idx = round(prob * pointcloud.shape[0]) - 1
+        pointcloud = pointcloud[:idx]
+        return pointcloud
+
+    def drop(self, pointcloud, prob = 0):
+        axis = np.random.randint(0,3)
+        low = np.min(pointcloud[:,axis])
+        high = np.max(pointcloud[:,axis])
+        selected_points = []
+        for point in pointcloud:
+            # draw random number base on x (or y z) position of the point
+            dist_to_low = ((point[axis] - low) / (high - low))
+            prob_accept = prob + (1 - prob) * dist_to_low
+            if np.random.uniform() < prob_accept:
+                selected_points.append(point)
+        pointcloud = np.asarray(selected_points)
+        return pointcloud
 
 if __name__ == '__main__':
-    train = ModelNet40(1024)
-    test = ModelNet40(1024, 'test')
-    for data, label in train:
-        print(data.shape)
-        print(label.shape)
+    dataset = ModelNet40(operation='drop')
+
+    wandb.login()
+    wandb.init(
+        # Set the project where this run will be logged
+        project=f'gnn-on-pointcloud',
+        group='test',
+    )
+
+    dataset.visualize()
+
+    wandb.finish()
